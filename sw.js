241f9b71c479a38f9a6bdb434f652d6aa0c85f52
salkhon <salmankhondker1998@gmail.com>
2023-11-11 01:54:19 +0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d323ded580f000c14460.js"
+    "url": "webpack-runtime-0849d47ebe1a4625d1b8.js"
   },
   {
     "url": "framework-025a10830d88a05b2804.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b5fffd0cc3a291df856c6085f37adec0"
+    "revision": "38a768700be97e8d80512d7ff93d3261"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9b1997f2483a747a5203.js"
