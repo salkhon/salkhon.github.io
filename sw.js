b22544b616026f13382c006fc25378e026a619a8
salkhon <salkhon050@gmail.com>
2026-02-08 01:05:02 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e5641bd6a8936405b80c.js"
+    "url": "webpack-runtime-6044b440a277330addef.js"
   },
   {
     "url": "framework-025a10830d88a05b2804.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8fb9c89b7765d0f8e18df12a1f736f19"
+    "revision": "663a8b5a102b1aee6931bb5e886a50a4"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9b1997f2483a747a5203.js"
